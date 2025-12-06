819e39482c72a53ecad9909061d531c65eb31be1
Chalana De Silva <chalanaddsilva@gmail.com>
2025-12-06 13:45:12 -0700

    Add license and header comments to the sketch

--- serial_button_keyboard.ino
+++ serial_button_keyboard.ino
@@ -1,3 +1,29 @@
+//  ____            _       _   ____        _   _              
+// / ___|  ___ _ __(_) __ _| | | __ ) _   _| |_| |_ ___  _ __  
+// \___ \ / _ \ '__| |/ _` | | |  _ \| | | | __| __/ _ \| '_ \ 
+//  ___) |  __/ |  | | (_| | | | |_) | |_| | |_| || (_) | | | |
+// |____/ \___|_|  |_|\__,_|_| |____/ \__,_|\__|\__\___/|_| |_|
+// | |/ /___ _   _| |__   ___   __ _ _ __ __| |                
+// | ' // _ \ | | | '_ \ / _ \ / _` | '__/ _` |                
+// | . \  __/ |_| | |_) | (_) | (_| | | | (_| |                
+// |_|\_\___|\__, |_.__/ \___/ \__,_|_|  \__,_|                
+//           |___/                                             
+
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <Keyboard.h>
 
 String storedText[3] = {"", "", ""}; // Stores text for each button
